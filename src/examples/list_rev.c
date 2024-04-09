03b2671ad79e27044acd25f8f6f9aedb5375014d
Christopher Pulte <cp526@cam.ac.uk>
2024-04-09 17:55:47 +0100

    list_rev with lemma

---
+++ src/examples/list_rev.c
@@ -0,0 +1,49 @@
+#include "list.h"
+#include "append.h"
+
+/*@
+function [rec] (datatype seq) snoc(datatype seq xs, i32 y) {
+  match xs {
+    Seq_Nil {} => {
+      Seq_Nil {}
+    }
+    Seq_Cons {head : h, tail : zs}  => {
+      snoc (rev(zs), h)
+    }
+  }
+}
+
+function [rec] (datatype seq) rev(datatype seq xs) {
+  match xs {
+    Seq_Nil {} => {
+      Seq_Nil {}
+    }
+    Seq_Cons {head : h, tail : zs}  => {
+      snoc (rev(zs), h)
+    }
+  }
+}
+
+lemma append_nil (datatype seq l1)
+requires true
+ensures append(l1, Seq_Nil {}) == l1
+
+@*/
+
+struct int_list* IntList_rev_aux(struct int_list* xs, struct int_list* ys)
+/*@ trusted @*/
+/*@ requires take L1 = IntList(xs) @*/
+/*@ requires take L2 = IntList(ys) @*/
+/*@ ensures take R = IntList(return) @*/
+/*@ ensures R == append(rev(L2), L1) @*/
+{
+}
+
+struct int_list* IntList_rev(struct int_list* xs, int y)
+/*@ requires take L1 = IntList(xs) @*/
+/*@ ensures take L1_rev = IntList(return) @*/
+/*@ ensures L1_rev == rev(L1) @*/
+{
+  /*@ apply append_nil(rev(L1)); @*/
+  return IntList_rev_aux (0, xs);
+}
