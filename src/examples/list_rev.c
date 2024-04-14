51d1ccdefdada69887a734e4ef367b7257594d3c
Christopher Pulte <cp526@cam.ac.uk>
2024-04-14 20:00:53 +0100

    add looping variant of list reverse as possible exercise

--- src/examples/list_rev.c
+++ src/examples/list_rev.c
@@ -30,10 +30,10 @@ lemma append_nil_r (datatype seq l1)
 requires true
 ensures append(l1, Seq_Nil {}) == l1
 
-lemma append_cons_r (datatype seq l1, i32 l2_hd, datatype seq l2_tl)
+lemma append_cons_r (datatype seq l1, i32 x, datatype seq l2)
 requires true
-ensures append(l1, Seq_Cons {head: l2_hd, tail: l2_tl})
-        == append(snoc(l1, l2_hd), l2_tl)
+ensures append(l1, Seq_Cons {head: x, tail: l2})
+        == append(snoc(l1, x), l2)
 @*/
 
 /*
@@ -42,7 +42,7 @@ HAVE append(rev(L2_tl), cons(L2_hd, L1))
        ==                                (by append_cons_r)
      append(snoc(rev(L2_tl),L2_hd), L1) 
        ==                                (by definition of rev)
-NEED append(rev(cons(L2_hd, L2_tl)), l1)
+NEED append(rev(cons(L2_hd, L2_tl)), L1)
 */
 
 
@@ -76,3 +76,49 @@ struct int_list* IntList_rev(struct int_list* xs)
 
 
 
+
+
+struct int_list* IntList_rev_loop(struct int_list *xs)
+/*@ requires take L = IntList(xs)
+    ensures  take L_ = IntList(return);
+             L_ == rev(L)
+@*/
+{
+  struct int_list *last = 0;
+  struct int_list *cur = xs;
+  /*@ apply append_nil_r(rev(L)); @*/
+  while(1) 
+  /*@ inv take L1 = IntList(last);
+          take L2 = IntList(cur);
+          append(rev(L2), L1) == rev(L)
+  @*/
+  {
+    if (cur == 0) {
+      /*@ unfold rev(Seq_Nil {}); @*/
+      /*@ unfold append(Seq_Nil {}, L1); @*/
+      return last;
+    }
+    struct int_list *tmp = cur->tail;
+    cur->tail = last;
+    last = cur;
+    cur = tmp;
+    /*@ unfold rev(L2); @*/
+    /*@ apply append_cons_r (rev (tl(L2)), hd(L2), L1); @*/
+  }
+}
+
+
+
+
+
+/*
+append(rev(L2), L1) 
+
+by def rev(L2)
+
+append(snoc (rev (tl(L2)), hd(L2)), L1) 
+
+by append_cons_r (rev (tl(L2)), hd(L2), L1)
+
+append(rev (tl(L2)), Seq_Cons {head: hd(L2), tail: L1})
+*/
