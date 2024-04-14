18a83c296b4ec0df6cd5cf93068a801546eff73e
Christopher Pulte <cp526@cam.ac.uk>
2024-04-14 16:15:01 +0100

    fill in details of slf_sized_stack, use record type instead of datatype

--- src/examples/slf_sized_stack.c
+++ src/examples/slf_sized_stack.c
@@ -1,49 +1,5 @@
 #include "list.h"
-
-// -----------------------------------------------------------------------
-// BCP: Should really #include this from bcp_length.c, not repeat it!
-//
-// But not sure what's the cleanest way to do that -- need to address
-// the multiple include problem...
-
-/* --BEGIN-- */
-/*@
-function [rec] (u32) length(datatype seq xs) {
-  match xs {
-    Seq_Nil {} => {
-      0u32
-    }
-    Seq_Cons {head : h, tail : zs}  => {
-      1u32 + length(zs)
-    }
-  }
-}
-@*/
-
-/* --END-- */
-unsigned int IntList_length (struct int_list *xs)
-/* --BEGIN-- */
-/*@ requires take L1 = IntList(xs)
-    ensures take L1_ = IntList(xs);
-            L1 == L1_;
-            return == length(L1)
-@*/
-/* --END-- */
-{
-  if (xs == 0) {
-/* --BEGIN-- */
-    /*@ unfold length(L1); @*/
-/* --END-- */
-    return 0;
-  } else {
-/* --BEGIN-- */
-    /*@ unfold length(L1); @*/
-/* --END-- */
-    return 1 + IntList_length (xs->tail);
-  }
-}
-
-// -----------------------------------------------------------------------
+#include "bcp_length.c"
 
 struct sized_stack {
   unsigned int size;
@@ -51,62 +7,102 @@ struct sized_stack {
 };
 
 /*@
-datatype sizeAndData {
-  SD {u32 s, datatype seq d}
-}
+type_synonym sizeAndData = {u32 s, datatype seq d}
 
-predicate (datatype sizeAndData) SizedStack(pointer p) {
+predicate (sizeAndData) SizedStack(pointer p) {
     take S = Owned<struct sized_stack>(p);
     let s = S.size;
     take d = IntList(S.data);
     assert(s == length(d));
-    return (SD { s: s, d: d });
+    return { s: s, d: d };
 }
 @*/
 
+extern struct sized_stack *malloc_sized_stack ();
+/*@
+spec malloc_sized_stack()
+     requires true
+     ensures take u = Block<struct sized_stack>(return)
+@*/
+
+extern void *free_sized_stack (struct sized_stack *p);
+/*@
+spec free_sized_stack(pointer p)
+     requires take u = Block<struct sized_stack>(p)
+     ensures true
+@*/
+
+
+
+
 struct sized_stack* create()
-/*@ trusted @*/
-// BCP: Is there a less verbose way to write this?
 /*@ ensures take S = SizedStack(return);
-    match S {
-      SD {s:s, d:d} => { s == 0u32 }
-    }
+            S.s == 0u32
 @*/
 {
-  // Call malloc, initialize, etc.
+  struct sized_stack *p = malloc_sized_stack();
+  p->size = 0;
+  p->data = 0;
+  /*@ unfold length(Seq_Nil {}); @*/
+  return p;
 }
 
 unsigned int sizeOf (struct sized_stack *p)
 /*@ requires take S = SizedStack(p)
     ensures take S_ = SizedStack(p);
-    S_ == S;
-    match S {
-      SD {s:s, d:d} => { return == s }
-    }
+            S_ == S;
+            return == S.s
 @*/
 {
   return p->size;
 }
 
-struct sized_stack* push (struct sized_stack *p, int x)
-/*@ trusted @*/
-// BCP: The following is pretty hideous.  Am I doing it wrong?
+void push (struct sized_stack *p, int x)
 /*@ requires take S = SizedStack(p)
     ensures take S_ = SizedStack(p);
-    match S {
-      SD {s:s, d:d} => {
-        match S_ {
-          SD {s:s_, d:d_} => {
-            d_ == Seq_Cons {head:x, tail:d}
-          }
-        }
-      }
-    }
+            S_.d == Seq_Cons {head:x, tail:S.d}
 @*/
 {
-  // Once again, I should write the actual code here, but it involves
-  // allocation...  I guess I need to just define a malloc and a free
-  // for the sized_stack type...
+  struct int_list *data = IntList_cons(x, p->data);
+  p->size++;
+  p->data = data;
+  /*@ unfold length (Seq_Cons {head: x, tail: S.d}); @*/
 }
 
-// BCP: We could add the pop and top operations...
+
+int pop (struct sized_stack *p)
+/*@ requires take S = SizedStack(p);
+             S.s > 0u32
+    ensures  take S_ = SizedStack(p);
+             S_.d == tl(S.d)
+@*/
+{
+  struct int_list *data = p->data;
+  if (data != 0) {
+    int head = data->head;
+    struct int_list *tail = data->tail;
+    IntList_free(data);
+    p->data = tail;
+    p->size--;
+    /*@ unfold length(S.d); @*/
+    return head;
+  }
+  return 42;
+}
+
+int top (struct sized_stack *p)
+/*@ requires take S = SizedStack(p);
+             S.s > 0u32
+    ensures  take S_ = SizedStack(p);
+             S_ == S;
+             return == hd(S.d)
+@*/
+{
+  /*@ unfold length(S.d); @*/ //from S.s > 0u32 follows that the 'else' branch is impossible
+  if (p->data != 0) {
+    return (p->data)->head;
+  }
+  else {
+    return 42; // provably-dead code
+  }
+}
