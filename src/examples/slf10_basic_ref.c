f5eb162b052d4a2bdc626fb2719fa0f3f433b69d
Christopher Pulte <cp526@cam.ac.uk>
2024-04-14 16:44:53 +0100

    same for slf10_basic_ref.c

--- src/examples/slf10_basic_ref.c
+++ src/examples/slf10_basic_ref.c
@@ -1,6 +1,7 @@
-unsigned int *refUnsignedInt (unsigned int v)
-/*@ trusted
+extern unsigned int *refUnsignedInt (unsigned int v);
+/*@ spec refUnsignedInt(u32 v)
+    requires true
     ensures take vr = Owned(return);
             vr == v
 @*/
-{}
+
