e8e333cacd407fc6d29f21d72affdc96e7fb12db
Christopher Pulte <cp526@cam.ac.uk>
2024-04-14 16:30:59 +0100

    same for free.h

--- src/examples/free.h
+++ src/examples/free.h
@@ -1,15 +1,15 @@
 // free.h
 
-void freeInt (int *p)
-/*@ trusted
+extern void freeInt (int *p);
+/*@ spec freeInt(pointer p)
     requires take v = Block<int>(p)
     ensures true
 @*/
-{}
 
-void freeUnsignedInt (unsigned int *p)
-/*@ trusted
+
+extern void freeUnsignedInt (unsigned int *p);
+/*@ spec freeUnsignedInt(pointer p)
     requires take v = Block<unsigned int>(p)
     ensures true
 @*/
-{}
+
