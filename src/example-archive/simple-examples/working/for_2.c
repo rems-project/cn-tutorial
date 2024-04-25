67b88a586675b9df6f0b751113a260c76f40ff19
septract <miked@galois.com>
2024-04-25 10:17:37 -0700

    Move for_2.c example now it's fixed in CN

--- src/example-archive/simple-examples/working/for_2.c
+++ src/example-archive/simple-examples/working/for_2.c
@@ -5,7 +5,8 @@ int for_2()
   int acc = 0; 
   int i; 
   for(i = 0; i < 10; i++)
-  /*@ inv 0i32 <= i; i <= 10i32;
+  /*@ inv 0i32 <= i; 
+          i <= 10i32;
           acc <= 10i32 @*/
   {
     acc = i; 
--- src/example-archive/simple-examples/working/for_3.c
+++ src/example-archive/simple-examples/working/for_3.c
@@ -4,8 +4,9 @@
 int for_3() 
 {
   int acc = 0; 
-  for(int i = 0; i <= 10; i++) // <-- the problem is the variable declaration 
-  /*@ inv 0i32 <= i; i <= 10i32;
+  for(int i = 0; i < 10; i++) 
+  /*@ inv 0i32 <= i; 
+          i <= 10i32;
           acc <= 10i32 @*/
   {
     acc = i; 
