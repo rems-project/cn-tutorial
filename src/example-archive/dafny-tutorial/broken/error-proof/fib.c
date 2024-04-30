bd178a44647470426a952808d97b2ec1a41e3b8a
Christopher Pulte <cp526@cam.ac.uk>
2024-04-30 15:01:46 +0100

    fix some more examples for new semicolon-syntax

--- src/example-archive/dafny-tutorial/broken/error-proof/fib.c
+++ src/example-archive/dafny-tutorial/broken/error-proof/fib.c
@@ -13,38 +13,38 @@ function [rec] (integer) fib_spec(integer n)
 // https://github.com/rems-project/cerberus/blob/master/tests/cn/mutual_rec/mutual_rec.c
 
 lemma lemma_ord_fib_spec (integer a, integer b)
-  requires 0 <= a; a < b
-  ensures fib_spec(a) <= fib_spec(b)
+  requires 0 <= a; a < b;
+  ensures fib_spec(a) <= fib_spec(b);
 @*/
 
 int compute_fib_2(int n)
-/*@ requires 0 <= n @*/
-/*@ requires fib_spec(n) < power(2,31) @*/
-// /*@ ensures return == fib_spec(n) @*/
+/*@ requires 0 <= n; @*/
+/*@ requires fib_spec(n) < power(2,31); @*/
+// /*@ ensures return == fib_spec(n); @*/
 {
   int i = 1;
   int a = 0;
   int b = 1;
 
-  /*@ unfold fib_spec(0) @*/
+  /*@ unfold fib_spec(0); @*/
   if (n == 0)
   {
     return 0;
   };
 
-  /*@ unfold fib_spec(1) @*/
-  /*@ apply lemma_ord_fib_spec(1,n) @*/
+  /*@ unfold fib_spec(1); @*/
+  /*@ apply lemma_ord_fib_spec(1,n); @*/
 
   while (i < n)
-  /*@ inv {n}unchanged; n != 0 @*/
-  /*@ inv 0 < i; i <= n @*/
-  /*@ inv a == fib_spec(i - 1) @*/
-  /*@ inv b == fib_spec(i) @*/
-  /*@ inv 0 <= a; a < b; b <= fib_spec(n) @*/
-  /*@ inv i == n ? b == fib_spec(n) : (a + b) < power(2,31) @*/
+  /*@ inv {n}unchanged; n != 0; @*/
+  /*@ inv 0 < i; i <= n; @*/
+  /*@ inv a == fib_spec(i - 1); @*/
+  /*@ inv b == fib_spec(i); @*/
+  /*@ inv 0 <= a; a < b; b <= fib_spec(n); @*/
+  /*@ inv i == n ? b == fib_spec(n) : (a + b) < power(2,31); @*/
   {
-    /*@ unfold fib_spec(i) @*/
-    /*@ unfold fib_spec(i+1) @*/
+    /*@ unfold fib_spec(i); @*/
+    /*@ unfold fib_spec(i+1); @*/
     int tmp_a = a;
     a = b;
     b = tmp_a + b;
--- src/example-archive/dafny-tutorial/working/abs_3.c
+++ src/example-archive/dafny-tutorial/working/abs_3.c
@@ -3,11 +3,11 @@
 
 int abs_3(int x)
 /*@ requires 
-      x == (0i32 - 1i32) @*/ // TODO: syntax is bad 
+      x == (0i32 - 1i32); @*/ // TODO: syntax is bad 
 /*@ ensures 
       0i32 <= return;
       (return == x || return == (0i32 - x)); 
-      return == 1i32 @*/
+      return == 1i32; @*/
 {
   return x + 2;
 }
