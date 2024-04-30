73f4dfc3aac6854826e5d915b160686cbc6d8da5
Christopher Pulte <cp526@cam.ac.uk>
2024-04-30 15:05:03 +0100

    and one more

--- src/example-archive/SAW/broken/error-proof/00003.point.c
+++ src/example-archive/SAW/broken/error-proof/00003.point.c
@@ -110,13 +110,13 @@ bool point_eq(const point *p1, const point *p2)
 	        return == 1u8
 	     }else{
 	        return == 0u8
-	     }
+	     };
 @*/
 {
     return p1->x == p2->x && p1->y == p2-> y;
 }
 
-/*@ spec malloc()
+/*@ spec malloc();
     requires true; 
     ensures take v = Block<struct point>(return); 
 @*/
