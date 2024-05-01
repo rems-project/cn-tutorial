de1f64032bf9c279d890e7c352a6691e9446b1ca
Christopher Pulte <cp526@cam.ac.uk>
2024-05-01 21:15:53 +0100

    adjust check.sh for CI testing

--- check.sh
+++ check.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
+if [ -n "$1" ] 
+then
+    echo "using CN=$1"
+    CN="$1"
+else
+    CN=cn
+fi
+
+
 good=0
 bad=0
 
-for file in build/solutions/*c; do
+for file in src/examples/*c; do
     if [[ $file == *.broken.c ]] ; then
         echo "(skipping $file)";
-    else if cn $file ; then
+    else if $CN $file ; then
         good=$(($good+1))
     else 
         bad=$(($bad+1))
@@ -17,3 +26,11 @@ done
 echo "----------------------------"
 echo "$good good, $bad bad"
 echo "----------------------------"
+
+
+if [[ "$bad" = 0 ]]
+then
+  exit 0
+else
+  exit 1
+fi
