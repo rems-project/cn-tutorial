0fc565f0445083e29c4b9a7ecc7e2e7aff0329f2
septract <miked@galois.com>
2024-05-02 09:55:32 -0700

    add inc / dec examples

---
+++ src/example-archive/simple-examples/working/inc_1.c
@@ -0,0 +1,26 @@
+
+int inc_1_pre(int i) 
+/*@ requires 
+      let MAXi32 = 2147483647i64; 
+      (i64) i + 1i64 <  MAXi32; @*/
+/*@ ensures return == i + 1i32; @*/
+{ 
+  int start, pre, post; 
+  start = i; 
+  pre = ++i; 
+  assert(pre == start+1);
+  return i; 
+}
+
+int inc_1_post(int i) 
+/*@ requires 
+      let MAXi32 = 2147483647i64; 
+      (i64) i + 1i64 <  MAXi32; @*/
+/*@ ensures return == i + 1i32; @*/
+{ 
+  int start, pre, post; 
+  start = i; 
+  pre = i++; 
+  assert(pre == start);
+  return i; 
+}
