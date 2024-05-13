db7c62a22077315e15cbb5bda396d7fe7685f8ab
septract <miked@galois.com>
2024-05-12 21:38:19 -0700

    Add test cases derived from `c-testsuite`

---
+++ src/example-archive/simple-examples/working/effect_1.c
@@ -0,0 +1,34 @@
+// Should be provable but doesn't work
+// Cause: lack of frame preservation for global variables 
+// Derived from src/example-archive/c-testsuite/broken/error-proof/00033.err1.c
+
+int g;
+
+void
+write_g_to_1()
+/*@ requires 
+        take Pre = Block<int>(&g); @*/
+/*@ ensures 
+        take Post = Owned<int>(&g); 
+        Post == 1i32; @*/
+{
+	g = 1;
+}
+
+
+int
+effect_1()
+/*@ requires 
+      take Pre = Block<int>(&g); @*/
+/*@ ensures 
+      take Post = Owned<int>(&g); 
+      return == 1i32; @*/
+{
+  int x;
+  
+  g = 0;
+  write_g_to_1(); 
+  if(g)
+    return 1;
+  return 0; 
+} 
