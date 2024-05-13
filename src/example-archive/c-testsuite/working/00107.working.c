92d00795acb0827fda2a82ba6fefa14f1e581655
septract <miked@galois.com>
2024-05-12 21:23:01 -0700

    Add proofs to examples

--- src/example-archive/c-testsuite/working/00107.working.c
+++ src/example-archive/c-testsuite/working/00107.working.c
@@ -3,6 +3,9 @@ myint x = (myint)1;
 
 int
 main(void)
+/*@ accesses x; @*/
+/*@ requires x == 1i32; @*/
+/*@ ensures return == 0i32; @*/
 {
 	return x-1;
 }
--- src/example-archive/c-testsuite/working/00121.working.c
+++ src/example-archive/c-testsuite/working/00121.working.c
@@ -3,18 +3,21 @@ int f(int a), g(int a), a;
 
 int
 main()
+/*@ ensures return == 0i32; @*/
 {
 	return f(1) - g(1);
 }
 
 int
 f(int a)
+/*@ ensures return == a; @*/
 {
 	return a;
 }
 
 int
 g(int a)
+/*@ ensures return == a; @*/
 {
 	return a;
 }
