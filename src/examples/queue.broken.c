6a7d36ca9d69293d1d1120663c76fa25b64c02bf
Benjamin Pierce <bcpierce00t@gmail.com>
2024-06-09 12:28:48 -0400

    queue example working!

--- src/examples/queue.broken.c
+++ src/examples/queue.broken.c
@@ -44,6 +44,7 @@ predicate (datatype seq) IntQueueAux (pointer h, pointer t, i32 lastVal) {
     return (Seq_Cons{head: lastVal, tail: Seq_Nil{}});
   } else {
     take C = Owned<struct int_queueCell>(h);
+    assert (!is_null(C.next));  // HERE IS THE KEY!
     take TL = IntQueueAux(C.next, t, lastVal);
     return (Seq_Cons { head: C.first, tail: TL });
   }
