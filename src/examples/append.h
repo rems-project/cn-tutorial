a8aa46aa059d2266ad927cc6b4ffc9dae0ced00f
scuellar <cuellar.s@gmail.com>
2024-06-11 10:19:46 -0400

    Change recursive functions so lemmas can be generated.

--- src/examples/append.h
+++ src/examples/append.h
@@ -1,13 +1,17 @@
 // append.h
 
 /*@
-function [rec] (datatype seq) append(datatype seq xs, datatype seq ys) {
+function (datatype seq) append_hack(datatype seq xs, datatype seq ys) {
+  ys
+}
+  
+function (datatype seq) append(datatype seq xs, datatype seq ys) {
   match xs {
     Seq_Nil {} => {
       ys
     }
     Seq_Cons {head : h, tail : zs}  => {
-      Seq_Cons {head: h, tail: append(zs, ys)}
+      Seq_Cons {head: h, tail: append_hack(zs, ys)}
     }
   }
 }
--- src/examples/list_snoc_spec.h
+++ src/examples/list_snoc_spec.h
@@ -1,11 +1,15 @@
 /*@
-function [rec] (datatype seq) snoc(datatype seq xs, i32 y) {
+function (datatype seq) snoc_hack(datatype seq xs, i32 y) {
+Seq_Cons {head: y, tail: Seq_Nil{}}
+}
+
+function (datatype seq) snoc(datatype seq xs, i32 y) {
   match xs {
     Seq_Nil {} => {
       Seq_Cons {head: y, tail: Seq_Nil{}}
     }
     Seq_Cons {head: x, tail: zs}  => {
-      Seq_Cons{head: x, tail: snoc (zs, y)}
+      Seq_Cons{head: x, tail: snoc_hack (zs, y)}
     }
   }
 }
