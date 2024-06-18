b75703373724ddcc9790a0f20fdbec0cd3b6a645
Liz Austell <122940625+elaustell@users.noreply.github.com>
2024-06-18 11:33:24 -0400

    correct last line in valid_state

--- src/examples/bridge/valid_state.h
+++ src/examples/bridge/valid_state.h
@@ -5,6 +5,6 @@ function (bool) valid_state (struct State s) {
      (s.LightA == 1i32 || s.LightB == 1i32) &&
      (s.W_A >= 0i32 && s.W_B >= 0i32) &&
      (0i32 <= s.Cross_Counter && s.Cross_Counter <= 5i32) &&
-     (s.LightA != 1i32 || s.LightB != 1i32 || s.Cross_Counter == 0i32)
+     ((s.LightA != 1i32 || s.LightB != 1i32) || (s.Cross_Counter == 0i32))
 }
 @*/
