a6eeb40949e53ea7738ae1a1d15e4c6fc7d7f708
Liz Austell <elaustell@davidson.edu>
2024-06-24 12:27:09 -0400

    add constructors that go out from middle node

---
+++ src/examples/Linked_List/WIPlinklist2.c
@@ -0,0 +1,43 @@
+#include "../list.h"
+#include "../list_length.c"
+#include "../list_snoc.h"
+#include "../list_append.h"
+
+struct Node {
+  int data;  
+  struct Node* prev;
+  struct Node* next;
+};
+
+/*@ 
+predicate (datatype seq) LinkedList (pointer p) {
+    if (is_null(p)) {
+        return Seq_Nil{};
+    } else {
+        take N = Owned<struct Node>(p);
+        take first = OwnBackwards(N.prev);
+        take rest = OwnForwards(N.next);
+        return append(first, Seq_Cons{head: N.data, tail: rest});
+    }
+}
+
+predicate (datatype seq) OwnForwards(pointer p) {
+    if (is_null(p)) {
+        return Seq_Nil{};
+    } else {
+        take N = Owned<struct Node>(p);
+        take rest = OwnForwards(N.next);
+        return Seq_Cons{head: N.data, tail: rest};
+    }
+}
+
+predicate (datatype seq) OwnBackwards(pointer p) {
+    if (is_null(p)) {
+        return Seq_Nil{};
+    } else {
+        take N = Owned<struct Node>(p);
+        take first = OwnBackwards(N.prev);
+        return snoc(first, N.data);
+    }
+}
+@*/
