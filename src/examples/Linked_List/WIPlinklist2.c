36c37272e94737152d04b9e00c0a0be25f6d7368
Liz Austell <elaustell@davidson.edu>
2024-06-24 18:33:43 -0400

    try to implement add

--- src/examples/Linked_List/WIPlinklist2.c
+++ src/examples/Linked_List/WIPlinklist2.c
@@ -15,6 +15,8 @@ predicate (datatype seq) LinkedList (pointer p) {
         return Seq_Nil{};
     } else {
         take N = Owned<struct Node>(p);
+        // assert (is_null(N.prev) || N.prev.next == N);
+        // assert (is_null(N.next) || N.next.prev == N);
         take first = OwnBackwards(N.prev);
         take rest = OwnForwards(N.next);
         return append(first, Seq_Cons{head: N.data, tail: rest});
@@ -27,6 +29,7 @@ predicate (datatype seq) OwnForwards(pointer p) {
     } else {
         take N = Owned<struct Node>(p);
         take rest = OwnForwards(N.next);
+        // assert (is_null(N.next) || (*(N.next)).prev == N);
         return Seq_Cons{head: N.data, tail: rest};
     }
 }
@@ -36,6 +39,7 @@ predicate (datatype seq) OwnBackwards(pointer p) {
         return Seq_Nil{};
     } else {
         take N = Owned<struct Node>(p);
+        // assert (is_null(N.prev) || N.prev.next == N);
         take first = OwnBackwards(N.prev);
         return snoc(first, N.data);
     }
@@ -107,8 +111,40 @@ struct Node *add_between(int element, struct Node *prevNode, struct Node *nextNo
              take next = Owned<struct Node>(nextNode);
              take rest = OwnForwards(next.next);
              take first = OwnBackwards(prev.prev);
+             prev.next == nextNode;
+             next.prev == prevNode;
     ensures  take result = LinkedList(return);
              result == append(snoc(first,prev.data), Seq_Cons{head: element, tail: Seq_Cons{head: next.data, tail: rest}});
+             result != Seq_Nil{};
+@*/
+{
+    struct Node *newNode = mallocNode();
+
+    newNode->prev = prevNode;
+    newNode->data = element;
+    newNode->next = nextNode;
+
+    prevNode->next = newNode;
+    nextNode->prev = newNode;
+    /*@ unfold append(snoc(first,prev.data), Seq_Cons{head: element, tail: Seq_Cons{head: next.data, tail: rest}}); @*/
+    return newNode;
+}
+
+struct Node *add_between_worse(int element, struct Node *prevNode, struct Node *nextNode)
+/*@ requires !is_null(prevNode) && !is_null(nextNode);
+             take prev = Owned<struct Node>(prevNode);
+             take next = Owned<struct Node>(nextNode);
+             take rest = OwnForwards(next.next);
+             take first = OwnBackwards(prev.prev);
+            //  prev.next == nextNode;
+            //  next.prev == prevNode;
+    ensures  take prev_ = Owned<struct Node>(prevNode);
+             take next_ = Owned<struct Node>(nextNode);
+             take rest_ = OwnForwards(next_.next);
+             take first_ = OwnBackwards(prev.prev);
+             take u = Owned<struct Node>(return);
+    // ensures  take result = LinkedList(return);
+    //          result == append(snoc(first,prev.data), Seq_Cons{head: element, tail: Seq_Cons{head: next.data, tail: rest}});
 @*/
 {
     struct Node *newNode = mallocNode();
@@ -121,4 +157,27 @@ struct Node *add_between(int element, struct Node *prevNode, struct Node *nextNo
     nextNode->prev = newNode;
 
     return newNode;
+}
+
+struct Node *add_worse(int element, struct Node *prevNode)
+/*@ requires !is_null(prevNode);
+             take n = Owned<struct Node>(prevNode);
+             take prev = OwnBackwards(n.prev);
+             take rest = OwnForwards(n.next);
+    ensures  take n_ = Owned<struct Node>(prevNode);
+             take prev_ = OwnBackwards(n.prev);
+             take rest_ = OwnForwards(n.next);
+             take u = Owned<struct Node>(return);
+@*/
+{
+    if (prevNode->next != 0) {
+        return add_between_worse(element, prevNode, prevNode->next);
+    } else {
+        struct Node *newNode = mallocNode();
+        newNode->prev = prevNode;
+        newNode->data = element;
+        newNode->next = 0;
+        prevNode->next = newNode;
+        return newNode;
+    }
 }
