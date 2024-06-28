5fa692d0fa666fa22d684df20a0ae4418a901b0f
Liz Austell <elaustell@davidson.edu>
2024-06-28 16:17:47 -0400

    add simple functions with new dblList and dblListOption types

--- src/examples/Linked_List/WIPlinklist.c
+++ src/examples/Linked_List/WIPlinklist.c
@@ -12,8 +12,8 @@ struct Node {
 
 /*@
 datatype dblList {
-    opts {datatype dblListOption first, datatype dblListOption rest},
-    seqs {datatype seq f, struct Node n, datatype seq r}
+    FirstRest {datatype dblListOption first, datatype dblListOption rest},
+    FirstNodeRest {datatype dblListOption f, struct Node n, datatype dblListOption r}
 }
 
 datatype dblListOption {
@@ -23,7 +23,7 @@ datatype dblListOption {
 
 function (datatype seq) flatten(datatype dblList l) {
     match l { 
-        opts{ first: f, rest: r} => {
+        FirstRest{ first: f, rest: r} => {
             match f {
                 empty{} => { 
                     match r {
@@ -47,29 +47,51 @@ function (datatype seq) flatten(datatype dblList l) {
                 }
             }
         }
-        seqs {f: f, n: n, r: r} => {
-            append(rev(f), Seq_Cons{head: n.data, tail: r})
+        FirstNodeRest {f: f, n: n, r: r} => {
+            // append(rev(f), Seq_Cons{head: n.data, tail: r})
+            match f {
+                empty{} => {
+                    match r {
+                        empty{} => {
+                            Seq_Cons{head: n.data, tail: Seq_Nil{}}
+                        }
+                        nonEmpty{node: n2, tail: t} => {
+                            Seq_Cons{head: n.data, tail: Seq_Cons{head: n2.data, tail: t}}
+                        }
+                    }
+                }
+                nonEmpty{node: n1, tail: t1} => {
+                    match r {
+                        empty{} => {
+                            rev(Seq_Cons{head: n.data, tail: Seq_Cons{head: n1.data, tail: t1}})
+                        }
+                        nonEmpty{node: n2, tail: t2} => {
+                            append(rev(Seq_Cons{head: n.data, tail: Seq_Cons{head: n1.data, tail: t1}}), Seq_Cons{head: n2.data, tail: t2})
+                        }
+                    }
+                }
+            }
         } 
     } 
 }
 
 predicate (datatype dblList) LinkedList (pointer p) {
     if (is_null(p)) {
-        return opts{first: empty{}, rest: empty{}};
+        return FirstRest{first: empty{}, rest: empty{}};
     } else {
         take N = Owned<struct Node>(p);
-        take ret = LinkedListHelper(p,N);
+        take ret = LinkedListAux(p,N);
         return ret;
     }
 }
 
-predicate (datatype dblList) LinkedListHelper (pointer p, struct Node N) {
+predicate (datatype dblList) LinkedListAux (pointer p, struct Node N) {
     if (ptr_eq(N.next,p) && ptr_eq(N.prev,p)) {
-        return opts{first: empty{}, rest: empty{}};
+        return FirstRest{first: empty{}, rest: empty{}};
     } else {
         take first = OwnBackwards(N.prev, p, N);
         take rest = OwnForwards(N.next, p, N);
-        return opts{first: first, rest: rest};
+        return FirstNodeRest{f: first, n: N, r: rest};
     }
 }
 
@@ -96,6 +118,8 @@ predicate (datatype seq) OwnForwardsAux(pointer p, pointer PrevPointer, struct N
         return Seq_Cons{head: N.data, tail: rest};
     }
 }
+
+
 
 predicate (datatype dblListOption) OwnBackwards(pointer p, pointer NextPointer, struct Node NextNode) {
     if (is_null(p)) {
@@ -137,7 +161,7 @@ extern void freeNode (struct Node *p);
 
 struct Node *empty()
 /*@ ensures take ret = LinkedList(return);
-    // ret == dbl_list{first: Seq_Nil{}, rest: Seq_Nil{}};
+    ret == FirstRest{first: empty{}, rest: empty{}};
     flatten(ret) == Seq_Nil{};
 @*/
 {
@@ -145,6 +169,72 @@ struct Node *empty()
    n->data = 0;
    n->prev = n;
    n->next = n;
-//    /*@ unfold append(Seq_Nil{}, Seq_Nil{}); @*/
    return n;
+}
+
+struct Node *singleton(int element)
+/*@ ensures take ret = LinkedList(return);
+            flatten(ret) == Seq_Cons{head: element, tail: Seq_Nil{}};
+@*/
+{
+   struct Node *n = mallocNode();
+   n->data = element;
+   n->prev = 0;
+   n->next = 0;
+   return n;
+}
+
+// struct Node *foo() 
+// /*@ ensures take ret = Owned<struct Node>(return);
+// @*/
+// {
+//     struct Node *n = mallocNode();
+//     n->data = 0;
+//     n->prev = 0;
+//     n->next = 0;
+//     return n;
+// }
+
+// is it possible to make the pre and post conditions more concise?
+struct Node *add_between_verbose(int element, struct Node *prevNode, struct Node *nextNode)
+/*@ requires !is_null(prevNode) && !is_null(nextNode);
+             take prev = Owned<struct Node>(prevNode);
+             take next = Owned<struct Node>(nextNode);
+             take rest = LinkedListAux(nextNode, next);
+             take first = LinkedListAux(prevNode, prev);
+
+             ptr_eq(prev.next, nextNode);
+             ptr_eq(next.prev, prevNode);
+
+    ensures  take ret = Owned<struct Node>(return);
+             take prev_ = Owned<struct Node>(prevNode);
+             take next_ = Owned<struct Node>(nextNode);
+             take rest_ = LinkedListAux(nextNode, next);
+             take first_ = LinkedListAux(prevNode, prev);
+
+            ptr_eq(prev_.next, return);
+            ptr_eq(ret.prev, prevNode);
+            ptr_eq(ret.next, nextNode);
+            ptr_eq(next_.prev, return);
+            ptr_eq(prev_.prev, prev.prev);
+            ptr_eq(next_.next, next.next);
+
+            rest == rest_;
+            first == first_;
+            //  take l = LinkedListAux(return, ret);
+            //  result == append(snoc(first,prev.data), Seq_Cons{head: element, tail: Seq_Cons{head: next.data, tail: rest}});
+            //  result != Seq_Nil{};
+@*/
+{
+    struct Node *newNode = mallocNode();
+
+    newNode->prev = prevNode;
+    newNode->data = element;
+    newNode->next = nextNode;
+
+
+    prevNode->next = newNode;
+    nextNode->prev = newNode;
+    // /*@ unfold append(snoc(first,prev.data), Seq_Cons{head: element, tail: Seq_Cons{head: next.data, tail: rest}}); @*/
+    return newNode;
 }
