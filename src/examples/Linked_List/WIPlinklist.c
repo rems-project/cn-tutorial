12782f7da20a83d30648e8395e357e896af60ea7
Liz Austell <elaustell@davidson.edu>
2024-07-01 16:58:42 -0400

    more work

--- src/examples/Linked_List/WIPlinklist.c
+++ src/examples/Linked_List/WIPlinklist.c
@@ -21,6 +21,61 @@ datatype dblListOption {
     nonEmpty {struct Node node, datatype seq tail}
 }
 
+function  (struct Node) getNodeOption(dblListOption l) {
+    match l {
+        empty{} => {
+            default<struct Node>
+        }
+        nonEmpty{node: n2, tail: t} => {
+            n2
+        }
+    }
+}
+
+function (struct Node) getNode(dblList l) {
+    match l {
+        FirstRest{first: f, rest: r} => {
+            default<struct Node>
+        }
+        FirstNodeRest{f: f, n: n, r: r} => {
+            n
+        }
+    }
+}
+
+function (datatype dblListOption) getFirst(dblList l) {
+    match l {
+        FirstRest{first: f, rest: r} => {
+            f
+        }
+        FirstNodeRest{f: f, n: n, r: r} => {
+            f
+        }
+    }
+}
+
+function (datatype dblListOption) getRest(dblList l) {
+    match l {
+        FirstRest{first: f, rest: r} => {
+            r
+        }
+        FirstNodeRest{f: f, n: n, r: r} => {
+            r
+        }
+    }
+}
+
+// function (datatype seq) getFirstOption(dblListOption l) {
+//     match l {
+//         empty{} => {
+//             Seq_Nil{}
+//         }
+//         nonEmpty{node: n2, tail: t} => {
+//             t
+//         }
+//     }
+// }
+
 function (datatype seq) flatten(datatype dblList l) {
     match l { 
         FirstRest{ first: f, rest: r} => {
@@ -144,6 +199,27 @@ predicate (datatype seq) OwnBackwardsAux(pointer p, pointer NextPointer, struct 
         return Seq_Cons{head: N.data, tail: first};
     }
 }
+
+predicate (datatype dblListOption) OwnForwardsAlternate(pointer p) {
+    if (is_null(p)) {
+        return empty{};
+    } else {
+        take N = Owned<struct Node>(p);
+        take rest = OwnForwardsAux(N.next, p, N);
+        return nonEmpty{node: N, tail: rest};
+    }
+}
+
+predicate (datatype dblListOption) OwnBackwardsAlternate(pointer p) {
+    if (is_null(p)) {
+        return empty{};
+    } else {
+        take N = Owned<struct Node>(p);
+        take first = OwnBackwardsAux(N.prev, p, N);
+        return nonEmpty{node: N, tail: first};
+    }
+}
+
 @*/
 
 extern struct Node *mallocNode();
@@ -184,17 +260,6 @@ struct Node *singleton(int element)
    return n;
 }
 
-// struct Node *foo() 
-// /*@ ensures take ret = Owned<struct Node>(return);
-// @*/
-// {
-//     struct Node *n = mallocNode();
-//     n->data = 0;
-//     n->prev = 0;
-//     n->next = 0;
-//     return n;
-// }
-
 // is it possible to make the pre and post conditions more concise?
 struct Node *add_between_verbose(int element, struct Node *prevNode, struct Node *nextNode)
 /*@ requires !is_null(prevNode) && !is_null(nextNode);
@@ -212,18 +277,18 @@ struct Node *add_between_verbose(int element, struct Node *prevNode, struct Node
              take rest_ = LinkedListAux(nextNode, next);
              take first_ = LinkedListAux(prevNode, prev);
 
-            ptr_eq(prev_.next, return);
-            ptr_eq(ret.prev, prevNode);
-            ptr_eq(ret.next, nextNode);
-            ptr_eq(next_.prev, return);
-            ptr_eq(prev_.prev, prev.prev);
-            ptr_eq(next_.next, next.next);
-
-            rest == rest_;
-            first == first_;
-            //  take l = LinkedListAux(return, ret);
-            //  result == append(snoc(first,prev.data), Seq_Cons{head: element, tail: Seq_Cons{head: next.data, tail: rest}});
-            //  result != Seq_Nil{};
+             ptr_eq(prev_.next, return);
+             ptr_eq(ret.prev, prevNode);
+             ptr_eq(ret.next, nextNode);
+             ptr_eq(next_.prev, return);
+             ptr_eq(prev_.prev, prev.prev);
+             ptr_eq(next_.next, next.next);
+ 
+             rest == rest_;
+             first == first_;
+             //  take l = LinkedListAux(return, ret);
+             //  result == append(snoc(first,prev.data), Seq_Cons{head: element, tail: Seq_Cons{head: next.data, tail: rest}});
+             //  result != Seq_Nil{};
 @*/
 {
     struct Node *newNode = mallocNode();
@@ -239,7 +304,7 @@ struct Node *add_between_verbose(int element, struct Node *prevNode, struct Node
     return newNode;
 }
 
-struct Node *add_between_WIP(int element, struct Node *prevNode, struct Node *nextNode)
+struct Node *add_between_verbose2(int element, struct Node *prevNode, struct Node *nextNode)
 /*@ requires !is_null(prevNode) && !is_null(nextNode);
              take prev = Owned<struct Node>(prevNode);
              take next = Owned<struct Node>(nextNode);
@@ -253,14 +318,13 @@ struct Node *add_between_WIP(int element, struct Node *prevNode, struct Node *ne
              take next_ = Owned<struct Node>(nextNode);
              take l_ = LinkedListAux(nextNode, next);
 
-            ptr_eq(prev_.next, return);
-            ptr_eq(ret.prev, prevNode);
-            ptr_eq(ret.next, nextNode);
-            ptr_eq(next_.prev, return);
-            ptr_eq(prev_.prev, prev.prev);
-            ptr_eq(next_.next, next.next);
+             ptr_eq(prev_.next, return);
+             ptr_eq(ret.prev, prevNode);
+             ptr_eq(ret.next, nextNode);
+             ptr_eq(next_.prev, return);
+             ptr_eq(prev_.prev, prev.prev);
+             ptr_eq(next_.next, next.next);
 
-            // needs something about relationship between l and l_
             // flatten(l_) != Seq_Nil{};
             // length = length + 1 ??
 @*/
@@ -274,6 +338,103 @@ struct Node *add_between_WIP(int element, struct Node *prevNode, struct Node *ne
 
     prevNode->next = newNode;
     nextNode->prev = newNode;
-    // /*@ unfold append(snoc(first,prev.data), Seq_Cons{head: element, tail: Seq_Cons{head: next.data, tail: rest}}); @*/
+
     return newNode;
+}
+
+// struct Node *add_between_WIP(int element, struct Node *prevNode, struct Node *nextNode)
+// /*@ requires !is_null(prevNode) && !is_null(nextNode);
+//              take l = LinkedList(prevNode);
+//              let prev = getNode(l);
+//              let next = getNodeOption(getRest(l));
+             
+//              ptr_eq(prev.next, nextNode);
+//              ptr_eq(next.prev, prevNode);
+
+//     ensures  take ret = Owned<struct Node>(return);
+//              take prev_ = Owned<struct Node>(prevNode);
+//              take next_ = Owned<struct Node>(nextNode);
+//              take l_ = LinkedListAux(nextNode, next);
+
+//              ptr_eq(prev_.next, return);
+//              ptr_eq(ret.prev, prevNode);
+//              ptr_eq(ret.next, nextNode);
+//              ptr_eq(next_.prev, return);
+//              ptr_eq(prev_.prev, prev.prev);
+//              ptr_eq(next_.next, next.next);
+
+//             // flatten(l_) != Seq_Nil{};
+//             // length = length + 1 ??
+// @*/
+// {
+//     struct Node *newNode = mallocNode();
+
+//     newNode->prev = prevNode;
+//     newNode->data = element;
+//     newNode->next = nextNode;
+
+
+//     prevNode->next = newNode;
+//     nextNode->prev = newNode;
+
+//     return newNode;
+// }
+
+int remove_helper(struct Node *node)
+/*@ requires take del = Owned<struct Node>(node);
+             !is_null(del.prev) || !is_null(del.next);
+             take first = Owned<struct Node>(del.prev);
+             take rest = Owned<struct Node>(del.next);   
+             take l = LinkedList(del.next);
+        
+    ensures  take first_ = Owned<struct Node>(del.prev);
+             take rest_ = Owned<struct Node>(del.next);   
+             take l_ = LinkedList(del.next);
+@*/ 
+{
+    struct Node *prev = node->prev;
+    struct Node *next = node->next;
+
+    prev->next = next;
+    next->prev = prev;
+
+    int data = node->data;
+    freeNode(node);
+    return data;
+}
+
+int remove(struct Node *n)
+/*@ requires take del = Owned<struct Node>(n);
+             take first = OwnForwardsAlternate(del.next);
+             take rest = OwnBackwardsAlternate(del.prev);
+             !is_null(del.prev) || !is_null(del.next);
+    ensures  take first_ = OwnForwardsAlternate(del.next);
+             take rest_ = OwnBackwardsAlternate(del.prev);
+             rest == rest_;
+             first == first_;
+@*/ 
+{
+    /*@ split_case(is_null(n)); @*/
+    /*@ split_case(is_null((*n).next)); @*/
+    /*@ split_case(is_null((*n).prev)); @*/
+
+
+    if (n->prev == 0) {
+        /*@ assert(is_null((*n).prev)); @*/
+        /*@ assert(!is_null((*n).next)); @*/
+
+        // n is the head
+        // n->next->prev = 0;
+    } else if (n->next == 0) {
+        /*@ assert(is_null((*n).next)); @*/
+        /*@ assert(!is_null((*n).prev)); @*/
+        // n is the tail
+    //    n->prev->next = 0;
+    } else {
+        // n->next->prev = 0;
+        // n->prev->next = 0;
+    }
+    int temp = n->data;
+    freeNode(n);
+    return temp;
 }
