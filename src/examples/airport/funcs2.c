8e612cb5d7b9242efe80c1f79a2090805743d4bc
Liz Austell <elaustell@davidson.edu>
2024-07-12 16:12:01 -0400

    Make airplane example work

--- src/examples/airport/funcs2.c
+++ src/examples/airport/funcs2.c
@@ -9,7 +9,15 @@ struct State increment_Plane_Counter(struct State s)
              0i32 <= s.Plane_Counter;
              s.Plane_Counter <= 2i32;
              s.ModeA == ACTIVE() || s.ModeD == ACTIVE();
+             s.ModeA == ACTIVE() implies s.W_D > 0i32;
+             s.ModeD == ACTIVE() implies s.W_A > 0i32;
     ensures  valid_state(return);
+             s.Runway_Time == return.Runway_Time;
+             s.ModeA == return.ModeA;
+             s.ModeD == return.ModeD;
+             s.W_A == return.W_A;
+             s.W_D == return.W_D;
+             s.Plane_Counter == return.Plane_Counter - 1i32;
 @*/
 /* --END-- */
 {
@@ -36,18 +44,64 @@ struct State reset_Plane_Counter(struct State s)
   return temp;
 }
 
+struct State increment_Runway_Time(struct State s)
+/* --BEGIN-- */
+/*@ requires valid_state(s);
+             0i32 <= s.Runway_Time;
+             s.Runway_Time <= 4i32; // TODO: might be 5?
+             s.ModeA == ACTIVE() || s.ModeD == ACTIVE();
+    ensures  valid_state(return);
+             s.Plane_Counter == return.Plane_Counter;
+             s.ModeA == return.ModeA;
+             s.ModeD == return.ModeD;
+@*/
+/* --END-- */
+{
+  struct State temp = s;
+  temp.Runway_Time = s.Runway_Time + 1;
+  return temp;
+}
+
+
+struct State reset_Runway_Time(struct State s)
+/* --BEGIN-- */
+/*@ requires valid_state(s);
+    ensures  valid_state(return);
+             return.Runway_Time == 0i32;
+             s.ModeA == return.ModeA;
+             s.ModeD == return.ModeD;
+             s.W_A == return.W_A;
+             s.W_D == return.W_D;
+             s.Plane_Counter == return.Plane_Counter;
+@*/
+/* --END-- */
+{
+  struct State temp = s;
+  temp.Runway_Time = 0;
+  return temp;
+}
+
 struct State arrive(struct State s)
 /* --BEGIN-- */
 /*@ requires valid_state(s);
-             s.ModeA == ACTIVE() && s.W_A >=1i32;
+             s.ModeA == ACTIVE() && s.W_A >= 1i32;
              s.Plane_Counter <= 2i32;
     ensures  valid_state(return);
+             s.Runway_Time == return.Runway_Time;
+             s.ModeA == return.ModeA;
+             s.ModeD == return.ModeD;
+             s.W_D == return.W_D;
+             s.W_D == 0i32 implies s.Plane_Counter == return.Plane_Counter;
+             s.W_D > 0i32 implies s.Plane_Counter == return.Plane_Counter - 1i32;
 @*/
 /* --END-- */
 {
   struct State temp = s;
   temp.W_A = s.W_A - 1;
-  return increment_Plane_Counter(temp);
+  if (s.W_D > 0) {
+    temp = increment_Plane_Counter(temp);
+  }
+  return temp;
 }
 
 struct State depart(struct State s)
@@ -56,22 +110,34 @@ struct State depart(struct State s)
              s.ModeD == ACTIVE() && s.W_D >=1i32;
              s.Plane_Counter <= 2i32;
     ensures  valid_state(return);
+              s.Runway_Time == return.Runway_Time;
+              s.ModeA == return.ModeA;
+              s.ModeD == return.ModeD;
+              s.W_A == return.W_A;
 @*/
 /* --END-- */
 {
   struct State temp = s;
   temp.W_D = s.W_D - 1;
-  return increment_Plane_Counter(temp);
+  if (s.W_A > 0) {
+    temp = increment_Plane_Counter(temp);
+  }
+  return temp;
 }
 
 struct State switch_modes(struct State s) 
 /* --BEGIN-- */
 /*@ requires valid_state(s);
              s.ModeA == ACTIVE() || s.ModeD == ACTIVE();
+             s.Plane_Counter == 0i32;
     ensures  valid_state(return);
              return.ModeA == ACTIVE() || return.ModeD == ACTIVE();
+             return.ModeA == s.ModeD;
+             return.ModeD == s.ModeA;
              return.W_A == s.W_A;
              return.W_D == s.W_D;
+             return.Runway_Time == s.Runway_Time;
+             s.Plane_Counter == return.Plane_Counter;
 @*/
 /* --END-- */
 {
@@ -93,48 +159,88 @@ struct State switch_modes(struct State s)
   return temp;
 }
 
-// struct State tick(struct Next_Car next, struct State s)
-// /* --BEGIN-- */
-// /*@ requires valid_state(s);
-//              (i64) s.Plane_Counter < 2147483647i64;
-//              (i64) s.W_A < 2147483647i64;
-//              (i64) s.W_D < 2147483647i64;
-//     ensures valid_state(return);
-// @*/
-// /* --END-- */
-// {
-//   struct State temp = s;
-//   if (next.A == 1) { temp = increment_W_A(temp); }
-//   if (next.B == 1) { temp = increment_W_B(temp); }
 
-//   // if one side has cars waiting, but the other side has no cars waiting
-//   if (((temp.W_A == 0) || (temp.W_B == 0)) && (((temp.W_A !=0) || (temp.W_B !=0)))) {
-//       temp = reset_Cross_Counter(temp);
-//       if (temp.W_A > 0) {
-//         if (temp.LightA == INACTIVE) {
-//             temp.LightA = ACTIVE;
-//             temp.LightB = INACTIVE;
-//         }
-//         temp.W_A = temp.W_A - 1;
-//         temp = increment_Cross_Counter(temp);
-//       }
-//   } else if (temp.W_A > 0 || temp.W_B > 0) {
-//     // Car waiting on both sides
-//     if (temp.LightA == INACTIVE && temp.LightB == INACTIVE) {
-//       // initial state, break the tie in favor of the A side
-//       temp.LightA = ACTIVE;
-//       temp.W_A = temp.W_A - 1;
-//       temp = increment_Cross_Counter(temp);
-//     } else {
-//       if (temp.Cross_Counter < 5) {
-//         temp = cross(temp);
-//       } else {
-//         temp = switch_lights(temp);
-//         temp = reset_Cross_Counter(temp);
-//         temp = cross(temp);
-//       }
+// This function represents what happens every minute at the airport. One `tick` corresponds to one minute.
+struct State tick(struct Next_Car next, struct State s)
+/* --BEGIN-- */
+/*@ requires valid_state(s);
+             (i64) s.Plane_Counter < 2147483647i64;
+             (i64) s.W_A < 2147483647i64;
+             (i64) s.W_D < 2147483647i64;
+    ensures valid_state(return);
+            (s.W_A > 0i32 && s.W_D == 0i32 && s.Runway_Time == 0i32 implies return.ModeA == ACTIVE());
+            (s.W_D > 0i32 && s.W_A == 0i32 && s.Runway_Time == 0i32 implies return.ModeD == ACTIVE());
+@*/
+/* --END-- */
+{
 
-//     }
-//   }
-//   return temp;
-// }
+    // Plane on the runway
+    if (s.Runway_Time > 0)
+    {
+      if (s.Runway_Time == 5)
+      {
+        s = reset_Runway_Time(s);
+      } else {
+        s = increment_Runway_Time(s);
+      }
+      return s;
+    }
+    // Plane chosen to arrive
+    else if (s.ModeA == ACTIVE && s.W_A > 0 && s.Plane_Counter < 3) {
+      s = arrive(s);
+      s = increment_Runway_Time(s); //todo
+      return s;
+    } 
+    // Plane chosen to depart
+    else if (s.ModeD == ACTIVE && s.W_D > 0 && s.Plane_Counter < 3) {
+      s = depart(s);
+      s = increment_Runway_Time(s); //todo
+      return s;
+    }
+    // Need to switch modes
+    else {
+      // Need to switch from arrival to departure mode
+      if (s.ModeA == ACTIVE) {
+        s = reset_Plane_Counter(s);
+        s = switch_modes(s);
+        // If there are planes waiting to depart, let one depart
+        if (s.W_D > 0) {
+          s = depart(s);
+          s = increment_Runway_Time(s); //todo
+        }
+        return s;
+      }
+      // Need to switch from departure to arrival mode
+      else if (s.ModeD == ACTIVE) {
+        s = reset_Plane_Counter(s);
+        s = switch_modes(s);
+
+
+        // If there are planes waiting to arrive, let one arrive
+        if (s.W_A > 0) {
+          s = arrive(s);
+          
+          s = increment_Runway_Time(s); //todo
+        }
+        return s;
+      }
+      // If neither mode is active, then it must be the beginning of the day.
+      else {
+        if (s.W_A > 0) {
+          s.ModeA = ACTIVE;
+          s = arrive(s);
+          s = increment_Runway_Time(s); //todo
+          return s;
+        } else if (s.W_D > 0) {
+          s.ModeD = ACTIVE;
+          s = depart(s);
+          s = increment_Runway_Time(s); //todo
+          return s;
+        } else {
+          // No planes are waiting, so we choose arrival mode and wait
+          s.ModeA = ACTIVE;
+          return s;
+        }
+      }
+    }
+}
--- src/examples/airport/valid_state.h
+++ src/examples/airport/valid_state.h
@@ -6,15 +6,11 @@ function (bool) valid_state (struct State s) {
      (s.W_A >= 0i32 && s.W_D >= 0i32) &&
      (0i32 <= s.Runway_Time && s.Runway_Time <= 5i32) && 
      (0i32 <= s.Plane_Counter && s.Plane_Counter <= 3i32) && 
-     (s.ModeA == INACTIVE() && s.ModeD == INACTIVE() implies s.Plane_Counter == 0i32)
+     (s.ModeA == INACTIVE() && s.ModeD == INACTIVE() implies s.Plane_Counter == 0i32) &&
+     (s.Runway_Time > 0i32 implies (s.ModeA == ACTIVE() || s.ModeD == ACTIVE())) &&
+
+     (s.Plane_Counter > 0i32 && s.ModeA == ACTIVE() implies s.W_D > 0i32) &&
+     (s.Plane_Counter > 0i32 && s.ModeD == ACTIVE() implies s.W_A > 0i32)
+
 }
 @*/
-
-
-// /*@ assert(temp.ModeA == INACTIVE() || temp.ModeA == ACTIVE()); @*/
-//   /*@ assert(temp.ModeD == INACTIVE() || temp.ModeD == ACTIVE()); @*/
-//   /*@ assert(temp.ModeA == INACTIVE() || temp.ModeD == INACTIVE()); @*/
-//   /*@ assert(temp.W_A >= 0i32 && temp.W_D >= 0i32); @*/
-//   /*@ assert(0i32 <= temp.Runway_Time && temp.Runway_Time <= 5i32); @*/
-//   /*@ assert(0i32 <= temp.Plane_Counter && temp.Plane_Counter <= 3i32); @*/ 
-//   /*@ assert(s.Plane_Counter == 0i32 implies s.ModeA == INACTIVE() && s.ModeD == INACTIVE()); @*/
