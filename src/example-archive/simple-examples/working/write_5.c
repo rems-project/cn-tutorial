c7aa17b4fe8272e3da4da981a8803db7e20321ce
septract <miked@galois.com>
2024-07-19 20:46:00 -0700

    Remove redundant instantiate, add stronger post to write_5 example

--- src/example-archive/simple-examples/working/write_5.c
+++ src/example-archive/simple-examples/working/write_5.c
@@ -6,7 +6,9 @@ void write_5(int *pair)
       take Cell2Pre = Owned(pair + 1i32); @*/
 /*@ ensures 
       take Cell1Post = Owned(pair);
-      take Cell2Post = Owned(pair + 1i32); @*/
+      take Cell2Post = Owned(pair + 1i32); 
+      Cell1Post == 7i32; 
+      Cell2Post == 8i32; @*/
 {
   pair[0] = 7;
   pair[1] = 8;
@@ -18,7 +20,10 @@ void write_5_alt(int *pair)
 /*@ requires 
       take PairPre = each (i32 j; j == 0i32 || j == 1i32) {Owned(pair + j)}; @*/
 /*@ ensures 
-      take PairPost = each (i32 j; j == 0i32 || j == 1i32) {Owned(pair + j)}; @*/
+      take PairPost = each (i32 j; j == 0i32 || j == 1i32) {Owned(pair + j)}; 
+      PairPost[0i32] == 7i32; 
+      PairPost[1i32] == 8i32; 
+      @*/
 {
   /*@ extract Owned<int>, 0i32; @*/
   pair[0] = 7;
