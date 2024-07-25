13db1ebbeb4a8509b4fade61fdf884b616062dbb
Liz Austell <elaustell@davidson.edu>
2024-07-25 18:34:27 -0400

    prove lemmas about predicates that go with proveable functions

--- src/examples/Dbl_Queue/lemmas.c
+++ src/examples/Dbl_Queue/lemmas.c
@@ -1,16 +1,74 @@
-#include "./WIP.c"
+#include "./predicates.c"
 
-void own_forwards_backwards(struct node *front, struct node *back)
+void own_forwards_append_lemma(struct node *front, struct node *second_last, struct node *last)
+/*@
+  requires
+      take F = Owned<struct node>(front);
+      take Second_last = Owned<struct node>(second_last);
+      take Fwd = Own_Forwards(F.next, front, F, second_last, Second_last);
+      ptr_eq(Second_last.next, last);
+      take Last = Owned<struct node>(last);
+      ptr_eq(Last.prev, second_last);
+  ensures
+      take F2 = Owned<struct node>(front);
+      take Last2 = Owned<struct node>(last);
+      take Fwd2 = Own_Forwards(F.next, front, F, last, Last2);
+      Fwd2 == snoc(Fwd, Last.data);
+      Last == Last2;
+      F2 == F;
+@*/
+{
+    /*@ unfold snoc(Seq_Nil{}, last->data); @*/
+    /*@ unfold snoc(Fwd, last->data); @*/
+
+    if (front->next == second_last) {
+        return;
+    } else {
+        own_forwards_append_lemma(front->next, second_last, last);
+        return;
+    }
+}
+
+void own_backwards_append_lemma(struct node *front, struct node *second_front, struct node *last)
+/*@
+  requires
+      take F = Owned<struct node>(front);
+      take Second_front = Owned<struct node>(second_front);
+      take Last = Owned<struct node>(last);
+      take Bwd = Own_Backwards(Last.prev, last, Last, second_front, Second_front);
+      ptr_eq(Second_front.prev, front);
+      ptr_eq(F.next, second_front);
+  ensures
+      take F2 = Owned<struct node>(front);
+      take Last2 = Owned<struct node>(last);
+      take Bwd2 = Own_Backwards(Last.prev, last, Last, front, F2);
+      Bwd2 == snoc(Bwd, F.data);
+      Last == Last2;
+      F2 == F;
+@*/
+{
+    /*@ unfold snoc(Seq_Nil{}, front->data); @*/
+    /*@ unfold snoc(Bwd, front->data); @*/
+
+    if (last->prev == second_front) {
+        return;
+    } else {
+        own_backwards_append_lemma(front, second_front, last->prev);
+        return;
+    }
+}
+
+void own_fwd_eq_bwd_lemma(struct node *front, struct node *back)
 /*@ requires
+            !ptr_eq(front, back);
+
             take B = Owned<struct node>(back);
             is_null(B.next);
+            !is_null(B.prev);
+            
             take F = Owned<struct node>(front);
-            // ptr_eq(F.next,back) implies ptr_eq(B.prev, front);
-            // ptr_eq(B.prev, front) implies ptr_eq(F.next,back);
+            !is_null(F.next);
 
-            // (is_null(F.next) && is_null(B.prev)) || (!is_null(F.next) && !is_null(B.prev));
-
-            // is_null(F.prev); // doesn't work because of recursive call
             take Fwd = Own_Forwards(F.next, front, F, back, B);
     ensures
             take B2 = Owned<struct node>(back);
@@ -18,7 +76,7 @@ void own_forwards_backwards(struct node *front, struct node *back)
             take F2 = Owned<struct node>(front);
             F == F2;
             take Bwd = Own_Backwards(B.prev, back, B, front, F);
-            Seq_Cons{head: F.data, tail: Fwd} == rev (Seq_Cons{head: B.data, tail: Bwd});
+            rev(Seq_Cons{head: F.data, tail: Fwd}) == Seq_Cons{head: B.data, tail: Bwd};
 @*/
 {
     if (front == 0) {
@@ -27,41 +85,90 @@ void own_forwards_backwards(struct node *front, struct node *back)
         if (front == back) {
             return;
         } else {
-            /*@ split_case(ptr_eq(front, back)); @*/
-            /*@ split_case(is_null((*front).next)); @*/
-            /*@ unfold rev(Seq_Cons {head: (*front).data, tail: Seq_Nil {}}); @*/
-            /*@ unfold rev(Seq_Nil{}); @*/
-            /*@ unfold snoc(Seq_Nil{}, (*front).data); @*/
             if (front->next == 0) {
                 return;
             } else {
                 if (front->next == back) {
-                    /*@ split_case(ptr_eq(front, back)); @*/
-                    /*@ split_case(is_null((*front).next)); @*/
-                    /*@ split_case(ptr_eq((*front).next, back)); @*/
-                    /*@ assert(ptr_eq((*back).prev, front)); @*/
+                    /*@ unfold rev(Seq_Nil{}); @*/
+                    /*@ unfold snoc(Seq_Nil{}, front->data); @*/
+
+                    /*@ unfold rev(Seq_Cons{head: back->data, tail: Seq_Nil{}}); @*/
+                    /*@ unfold snoc(Seq_Nil{}, back->data); @*/
+
+                    /*@ unfold rev(Seq_Cons{head: front->data, tail: Seq_Cons{head: back->data, tail: Seq_Nil{}}}); @*/
+
+                    /*@ unfold snoc(Seq_Cons{head: back->data, tail: Seq_Nil{}}, front->data); @*/
+                    
                     return;
                 } else {
-                    /*@ assert(!is_null((*front).next)); @*/
-                    /*@ assert(!is_null((*(*front).next).next)); @*/
-                    /*@ assert(ptr_eq((*(*front).next).prev, front)); @*/
-                    /*@ assert(!is_null((*back).prev)); @*/
-                    // /*@ assert(!is_null((*(*back).prev).prev)); @*/
-                    own_forwards_backwards(front->next, back);
-                    /*@ assert(!is_null((*front).next)); @*/
-                    /*@ assert(ptr_eq((*(*front).next).prev, front)); @*/
+                   
+                    own_fwd_eq_bwd_lemma(front->next, back);
+                   
+                    own_backwards_append_lemma(front, front->next, back);
+                    
 
-                    /*@ split_case(ptr_eq(front, back)); @*/
-                    /*@ split_case(is_null((*front).next)); @*/
-                    /*@ split_case(ptr_eq((*front).next, back)); @*/
+                    /*@ unfold rev(Seq_Cons{ head: front->data, tail: Fwd}); @*/
+                    /*@ unfold snoc(rev(Fwd), front->data); @*/
 
-                    /*@ split_case(ptr_eq((*(*front).next).next, back)); @*/
+                
+                    return;
+                }
+            }
+        }
+    }
+}
 
-                    /*@ split_case(ptr_eq((*back).prev, front)); @*/
+void own_bwd_eq_fwd_lemma(struct node *front, struct node *back)
+/*@ requires
+            !ptr_eq(front, back);
 
-                    // /*@ split_case(ptr_eq((*(*back).prev).prev, front)); @*/
+            take B = Owned<struct node>(back);
+            !is_null(B.prev);
+            
+            take F = Owned<struct node>(front);
+            !is_null(F.next);
+            is_null(F.prev);
 
+            take Bwd = Own_Backwards(B.prev, back, B, front, F);
+    ensures
+            take B2 = Owned<struct node>(back);
+            B == B2;
+            take F2 = Owned<struct node>(front);
+            F == F2;
+            take Fwd = Own_Forwards(F.next, front, F, back, B);
+            rev(Seq_Cons{head: B.data, tail: Bwd}) == Seq_Cons{head: F.data, tail: Fwd};
+@*/
+{
+    if (back == 0) {
+        return;
+    } else {
+        if (front == back) {
+            return;
+        } else {
+            if (back->prev == 0) {
+                return;
+            } else {
+                if (back->prev == front) {
+                    /*@ unfold rev(Seq_Nil{}); @*/
+                    /*@ unfold snoc(Seq_Nil{}, back->data); @*/
 
+                    /*@ unfold rev(Seq_Cons{head: front->data, tail: Seq_Nil{}}); @*/ 
+                    /*@ unfold snoc(Seq_Nil{}, front->data); @*/
+
+                    /*@ unfold rev(Seq_Cons{head: back->data, tail: Seq_Cons{head: front->data, tail: Seq_Nil{}}}); @*/
+
+                    /*@ unfold snoc(Seq_Cons{head: front->data, tail: Seq_Nil{}}, back->data); @*/
+                    
+                    return;
+                } else {
+                   
+                    own_bwd_eq_fwd_lemma(front, back->prev);
+                   
+                    own_forwards_append_lemma(front, back->prev, back);
+                    
+
+                    /*@ unfold rev(Seq_Cons{ head: back->data, tail: Bwd}); @*/ 
+                    /*@ unfold snoc(rev(Bwd), back->data); @*/
                     return;
                 }
             }
---
+++ src/examples/Dbl_Queue/predicates.c
@@ -0,0 +1,154 @@
+#include "../list_c_types.h"
+#include "../list_cn_types.h"
+#include "../list_hdtl.h"
+#include "../list_rev.h"
+
+struct dbl_queue {
+  struct node* front;  
+  struct node* back;
+};
+
+struct node {
+  int data;  
+  struct node* prev;
+  struct node* next;
+};
+
+/*@
+
+// This predicate owns the outer queue structure and 
+// asserts that the front and back pointers are either
+// both null or both point to a node. It then takes 
+// ownership of the nodes in the list in the forwards
+// direction through a call to FB_Forwards. It returns 
+// a sequence of integers representing the data in the queue.
+predicate (datatype seq) Dbl_Queue_Forwards (pointer l) {
+    if (is_null(l)) {
+        return Seq_Nil{};
+    }
+    else {
+        take L = Owned<struct dbl_queue>(l);
+        assert (   (is_null(L.front)  && is_null(L.back)) 
+                || (!is_null(L.front) && !is_null(L.back)));
+        take inner = FB_Forwards(L.front, L.back);
+        return inner;
+    }
+}
+
+
+// This predicate owns the front and back nodes in the queue, and then
+// takes calls Own_Forwards to take ownership of the rest of the nodes
+// in the queue. It asserts that the front node has a null prev pointer
+// and that the back node has a null next pointer. It then returns a
+// sequence of integers representing the data in the queue.
+predicate (datatype seq) FB_Forwards (pointer front, pointer back) {
+    if (is_null(front)) {
+        return Seq_Nil{};
+    } else {
+        if (ptr_eq(front, back)) {
+            // Only one node in the list, so only one node to own
+            take F = Owned<struct node>(front);
+            assert (is_null(F.next));
+            assert (is_null(F.prev));
+            return Seq_Cons {head: F.data, tail: Seq_Nil{}};
+        } else {
+            take B = Owned<struct node>(back);
+            assert (is_null(B.next));
+            take F = Owned<struct node>(front);
+            assert(is_null(F.prev));
+            take Rest = Own_Forwards(F.next, front, F, back, B);
+            return Seq_Cons{ head: F.data, tail: Rest};
+        }
+    }
+}
+
+// This recursive predicate takes ownership of a node in the queue, 
+// and includes checks that the queue is properly doubly linked. It
+// also asserts that any node owned in this predicate is not the front
+// or the back node. It calls itself recursively, walking forwards until
+// it reaches the last node in the list, which was already owned in the 
+// `FB_Forwards` predicate. The base case puts the back node into the sequence,
+// in order to avoid a call to `snoc`. This helps avoid future calls to `unfold`.
+predicate (datatype seq) Own_Forwards(pointer p, pointer prev_pointer, struct node prev_node, pointer b, struct node Back) {
+    if (ptr_eq(p,b)) {
+        assert(ptr_eq(Back.prev, prev_pointer));
+        return Seq_Cons{head: Back.data, tail: Seq_Nil{}};
+    } else {
+        take n = Owned<struct node>(p);
+        assert (ptr_eq(n.prev, prev_pointer));
+        assert(ptr_eq(prev_node.next, p));
+        assert(!is_null(n.next));
+        assert(!is_null(n.prev));
+        take Rest = Own_Forwards(n.next, p, n, b, Back);
+        return Seq_Cons{head: n.data, tail: Rest};
+    }
+}
+
+// This predicate owns the outer queue structure and 
+// asserts that the front and back pointers are either
+// both null or both point to a node. It then takes 
+// ownership of the nodes in the list in the backwards
+// direction through a call to FB_Backwards. It returns 
+// a sequence of integers representing the data in the queue
+// BACKWARDS. It returns the reverse of a call to Dbl_Queue_Forwards.
+predicate (datatype seq) Dbl_Queue_Backwards (pointer l) {
+    if (is_null(l)) {
+        return Seq_Nil{};
+    }
+    else {
+        take L = Owned<struct dbl_queue>(l);
+        assert (   (is_null(L.front)  && is_null(L.back)) 
+                || (!is_null(L.front) && !is_null(L.back)));
+        take inner = FB_Backwards(L.front, L.back);
+        return inner;
+    }
+}
+
+// This predicate owns the front and back nodes in the queue, and then
+// takes calls Own_Backwards to take ownership of the rest of the nodes
+// in the queue. It asserts that the front node has a null prev pointer
+// and that the back node has a null next pointer. It then returns a
+// sequence of integers representing the data in the queue BACKWARDS.
+predicate (datatype seq) FB_Backwards (pointer front, pointer back) {
+    if (is_null(front)) {
+        return Seq_Nil{};
+    } else {
+        if (ptr_eq(front, back)) {
+            take B = Owned<struct node>(back);
+            assert (is_null(B.next));
+            assert (is_null(B.prev));
+            return Seq_Cons {head: B.data, tail: Seq_Nil{}};
+        } else {
+            take B = Owned<struct node>(back);
+            assert (is_null(B.next));
+            take F = Owned<struct node>(front);
+            assert(is_null(F.prev));
+            take Rest = Own_Backwards(B.prev, back, B, front, F);
+            return Seq_Cons{ head: B.data, tail: Rest};
+        }
+    }
+}
+
+// This recursive predicate takes ownership of a node in the queue, 
+// and includes checks that the queue is properly doubly linked. It
+// also asserts that any node owned in this predicate is not the front
+// or the back node. It calls itself recursively, walking backwards until
+// it reaches the first node in the list, which was already owned in the 
+// `FB_Backwards` predicate. The base case puts the front node into the sequence,
+// in order to avoid a call to `snoc`. This helps avoid future calls to `unfold`.
+// Note that the front is put in the back because this sequence is backwards.
+predicate (datatype seq) Own_Backwards(pointer p, pointer next_pointer, struct node next_node, pointer f, struct node Front) {
+    if (ptr_eq(p,f)) {
+        assert(ptr_eq(Front.next, next_pointer));
+        return Seq_Cons{head: Front.data, tail: Seq_Nil{}};
+    } else {
+        take n = Owned<struct node>(p);
+        assert (ptr_eq(n.next, next_pointer));
+        assert(ptr_eq(next_node.prev, p));
+        assert(!is_null(n.next));
+        assert(!is_null(n.prev));
+        take Rest = Own_Backwards(n.prev, p, n, f, Front);
+        return Seq_Cons{head: n.data, tail: Rest};
+    }
+}
+@*/
