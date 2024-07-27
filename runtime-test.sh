cfb99dc9ce92e68df2ad49e428a64e97402eb53f
Dhruv Makwana <dc_mak@outlook.com>
2024-07-27 12:22:08 +0100

    Add runtime-test.sh based on Mike's script

---
+++ runtime-test.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+set -euo pipefail -o noclobber
+
+CHECK_SCRIPT="$HOME/cerberus/tests/cn-runtime/cn-runtime-single-file.sh"
+
+SCRIPT_OPT="-oq"
+
+SUCCESS=$(find src/examples -name '*.c' \
+    ! -name abs_mem_struct.c \
+    ! -name add_read.c \
+    ! -name "read.broken.c" \
+    ! -name slf14_basic_succ_using_incr_attempt.broken.c)
+
+BUGGY=("src/examples/abs_mem_struct.c" "src/examples/add_read.c")
+
+SHOULD_FAIL=("src/examples/read.broken.c" "src/examples/slf14_basic_succ_using_incr_attempt.broken.c")
+
+SUCCEEDED=""
+FAILED=""
+
+for FILE in ${SUCCESS}; do
+  echo "Testing ${FILE} ..."
+  "${CHECK_SCRIPT}" "${SCRIPT_OPT}" "${FILE}"
+  if [ $? -eq 0 ]; then
+    SUCCEEDED+=" ${FILE}"
+  else
+    FAILED+=" ${FILE}"
+  fi
+done
+
+for FILE in ${SHOULD_FAIL}; do
+  echo "Testing ${FILE} ..."
+  "${CHECK_SCRIPT}" "${SCRIPT_OPT}" "${FILE}"
+  if [ $? -ne 0 ]; then
+    SUCCEEDED+=" ${FILE}"
+  else
+    FAILED+=" ${FILE}"
+  fi
+done
+
+for FILE in ${BUGGY}; do
+  echo "Testing ${FILE} ..."
+  "${CHECK_SCRIPT}" "${SCRIPT_OPT}" "${FILE}"
+  if [ $? -ne 0 ]; then
+    SUCCEEDED+=" ${FILE}"
+  else
+    FAILED+=" ${FILE}"
+  fi
+done
+
+echo "Succeeded: $SUCCEEDED"
+echo "Failed: $FAILED"
