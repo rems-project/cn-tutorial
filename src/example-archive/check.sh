6cd930753406281694a13fbd25f47bf98249850d
Christopher Pulte <cp526@cam.ac.uk>
2024-08-15 10:57:33 +0100

    bump up timeout another time

--- src/example-archive/check.sh
+++ src/example-archive/check.sh
@@ -45,7 +45,7 @@ check_file() {
   local expected_exit_code=$2
 
   printf "[$file]... "
-  timeout 20 $CN "$file" > /dev/null 2>&1
+  timeout 25 $CN "$file" > /dev/null 2>&1
   local result=$?
 
   if [ $result -eq $expected_exit_code ]; then
