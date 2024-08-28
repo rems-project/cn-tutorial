701ea31eaf6d18d77ea8731bdd2778a62610c6ee
Rini Banerjee <rb2018@cam.ac.uk>
2024-08-28 18:33:14 +0100

    Update slf_incr2 to match #83

--- src/examples/slf_incr2.c
+++ src/examples/slf_incr2.c
@@ -16,10 +16,8 @@ predicate { u32 pv, u32 qv } BothOwned (pointer p, pointer q)
 void incr2 (unsigned int *p, unsigned int *q)
 /*@ requires take vs = BothOwned(p,q);
     ensures take vs_ = BothOwned(p,q);
-            ptr_eq (vs_.pv,
-                    (!ptr_eq(p,q)) ? (vs.pv + 1u32) : (vs.pv + 2u32));
-            ptr_eq (vs_.qv,
-                    (!ptr_eq(p,q)) ? (vs.qv + 1u32) : vs_.pv);
+            vs_.pv == (!ptr_eq(p,q) ? (vs.pv + 1u32) : (vs.pv + 2u32));
+            vs_.qv == (!ptr_eq(p,q) ? (vs.qv + 1u32) : vs_.pv);
 @*/
 {
   /*@ split_case ptr_eq(p,q); @*/
