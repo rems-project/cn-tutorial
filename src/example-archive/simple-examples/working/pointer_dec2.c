f284ac2ed1188d593ff425fea0787b1450f6d9e8
Dhruv Makwana <dc_mak@outlook.com>
2024-09-30 18:09:31 +0100

    Add support for VIP
    This commit is a backwards compatible change to some tests to enable VIP
    by default in CN in an upcoming commit.

--- src/example-archive/simple-examples/working/pointer_dec2.c
+++ src/example-archive/simple-examples/working/pointer_dec2.c
@@ -1,7 +1,7 @@
 // Derived from src/example-archive/c-testsuite/broken/error-proof/00032.err1.c
 
-int a;
+int a[2];
 void b() {
-  int *c = &a;
+  int *c = &a[1];
   --c;
 }
--- src/examples/queue_cn_types_3.h
+++ src/examples/queue_cn_types_3.h
@@ -4,7 +4,8 @@ predicate (datatype seq) IntQueueAux (pointer f, pointer b) {
     return Seq_Nil{};
   } else {
     take F = Owned<struct int_queueCell>(f);
-    assert (!is_null(F.next));  
+    assert (!is_null(F.next));
+    assert (ptr_eq(F.next, b) || !addr_eq(F.next, b));
     take B = IntQueueAux(F.next, b);
     return Seq_Cons{head: F.first, tail: B};
   }
