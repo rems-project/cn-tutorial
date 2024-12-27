c9706ff1565b6d2be13150b77612c2dc34fb2f47
Dhruv Makwana <dc-mak@users.noreply.github.com>
2024-12-27 21:52:03 +0000

    Re-add for rems-project/cerberus#797

---
+++ src/example-archive/c-testsuite/working/00051.c
@@ -0,0 +1,44 @@
+/*
+    Invalid_argument("option is None")
+*/
+// Cause: unknown 
+
+int x = 0;
+
+int
+main()
+/*@ accesses x; @*/
+{
+	switch(x)
+		case 0:
+			;
+	switch(x)
+		case 0:
+			switch(x) {
+				case 0:
+					goto next;
+				default:
+					return 1;
+			}
+	return 1;
+	next:
+	switch(x)
+		case 1:
+			return 1;
+	switch(x) {
+		{
+			x = 1 + 1;
+			foo:
+			case 1:
+				return 1;
+		}
+	}
+	switch(x) {
+		case 0:
+			return x;
+		case 1:
+			return 1;
+		default:
+			return 1;
+	}
+}
