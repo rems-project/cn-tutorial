82f1ec7479e42ca3c9279b053af3e5d871ed9b25
Benjamin Pierce <bcpierce@seas.upenn.edu>
2025-03-31 16:10:13 -0400

    Add mkdocs macros support

---
+++ main.py
@@ -0,0 +1,20 @@
+def define_env(env):
+    """
+    This is the hook for the variables, macros and filters.
+    """
+
+    def common(mesg, color):
+        "Format a TODO (common)"
+        return "<span style=\"color:" + color + "\">[[" + mesg + "]]</span>"
+
+    @env.macro
+    def todo(mesg):
+        "Format a TODO"
+        return common(mesg, "blue")
+
+    @env.macro
+    def later(mesg):
+        "Format a TODO for later"
+        return common(mesg, "lightgray")
+
+        
