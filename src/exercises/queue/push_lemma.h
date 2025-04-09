3115dfbffcb8452ba5f42c93ef852f010da4ffb6
Zain K Aamer <ZippeyKeys12@gmail.com>
2025-04-09 07:25:20 -0400

    Fix `push_lemma`, so it is true

--- src/exercises/queue/push_lemma.h
+++ src/exercises/queue/push_lemma.h
@@ -4,6 +4,7 @@ lemma push_lemma (pointer front, pointer p)
       ptr_eq(front, p) || !addr_eq(front, p);
       take Q = QueueAux(front, p);
       take P = RW<struct queue_cell>(p);
+      !is_null(P.next);
   ensures
       ptr_eq(front, P.next) || !addr_eq(front, P.next);
       take Q_post = QueueAux(front, P.next);
