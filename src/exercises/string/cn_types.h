48ec1b323f3b0288c64c85581b85bc0eb8785bf4
cassiatorczon <cassiatorczon@gmail.com>
2025-05-01 02:26:36 -0400

    mixed recursive style

--- src/exercises/string/cn_types.h
+++ src/exercises/string/cn_types.h
@@ -18,15 +18,10 @@ datatype String_Buf {
 }
 
 predicate (datatype String_Buf) String_Buf_At(pointer p, size_t buf_len) {
-    take s = String(p, buf_len);
-    return String_Buf { chars : s, buf_len : buf_len};
-}
-
-predicate (datatype String) String(pointer p, size_t buf_len) {
     assert (buf_len >= 1u64);
     take h = RW<char>(p);
     take s = String_Aux(p, buf_len, h);
-    return s;
+    return String_Buf { chars : s, buf_len : buf_len};
 }
 
 // p: pointer to h
@@ -34,14 +29,12 @@ predicate (datatype String) String(pointer p, size_t buf_len) {
 // h: first character of string starting at p
 predicate (datatype String) String_Aux(pointer p, size_t buf_len, u8 h) {
     if (h == 0u8) {
-        // TODO: initially I did not have "0u64 < i" but did not get any error
-        // indicating I was taking the same thing twice
         take rem = each (size_t i; 0u64 < i && i < buf_len - 1u64) {
             W<char>( array_shift<char>(p, i)) };
         return String_Nil {};
     } else {
         take tl_buf = String_Buf_At(array_shift<char>(p, 1u64), buf_len - 1u64);
-        return String_Cons {head : h, tail : tl};
+        return String_Cons {head : h, tail : chars(tl_buf)};
     }
 }
 
