7e204a86be46a2c9cdfed81620eea862661f436d
Rini Banerjee <rbanerjee20@gmail.com>
2026-09-14 14:45:33 +0100

    Add runtime-extras tests

---
+++ src/exercises/runtime-extras/same_func_malloc_free.c
@@ -0,0 +1,29 @@
+#ifndef CN_UTILS
+#include <stdlib.h>
+void *cn_malloc(unsigned long size);
+void cn_free_sized(void* p, unsigned long s);
+#endif
+
+unsigned int deref(unsigned int *p)
+/*@ trusted;
+requires
+    take v1_ = Owned(p);
+ensures
+    take v2 = Owned(p);
+    v1_ == v2;
+    return == v1_;
+@*/
+{
+  return *p;
+}
+
+int main()
+/*@ trusted; @*/
+{
+    unsigned int *p = cn_malloc(sizeof(unsigned int));
+    *p = 5;
+    unsigned int x = deref(p);
+    /*@ assert (x == 5u32); @*/
+    /*@ assert (*p == 5u32); @*/
+    cn_free_sized(p, sizeof(unsigned int));
+}
---
+++ src/exercises/runtime-extras/up_malloc_down_free.c
@@ -0,0 +1,27 @@
+#ifndef CN_UTILS
+#include <stdlib.h>
+void *cn_malloc(unsigned long size);
+void cn_free_sized(void* p, unsigned long s);
+#endif
+
+unsigned int *mkref(unsigned int x)
+/*@ trusted;
+requires
+    true;
+ensures
+    take v1_ = Owned(return);
+    v1_ == x;
+@*/
+{
+  unsigned int *p = cn_malloc(sizeof(unsigned int));
+  *p = x;
+  return p;
+}
+
+int main()
+/*@ trusted; @*/
+{
+    unsigned int *p = mkref(5);
+    /*@ assert (*p == 5u32); @*/
+    cn_free_sized(p, sizeof(unsigned int));
+}
