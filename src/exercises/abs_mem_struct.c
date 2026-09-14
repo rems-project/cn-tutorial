25faec557c934c335e08074e1a3878497fb71e51
Rini Banerjee <rbanerjee20@gmail.com>
2026-09-14 14:49:24 +0100

    [Fulm] Update abs examples

--- src/exercises/abs_mem_struct.c
+++ src/exercises/abs_mem_struct.c
@@ -4,7 +4,7 @@ int abs_mem (int *p)
              MINi32() < x;
     ensures take x2 = RW<int>(p);
             x == x2;
-            return == ((x >= 0i32) ? x : (0i32-x));
+            return == ((x >= 0) ? x : (0-x));
 @*/
 /* --END-- */
 {
@@ -30,8 +30,20 @@ int abs_y (struct tuple *p)
              MINi32() < s.y;
     ensures  take s2 = RW(p);
              s == s2;
-             return == ((s.y >= 0i32) ? s.y : (0i32-s.y));
+             return == ((s.y >= 0) ? s.y : (0-s.y));
 @*/
 {
   return abs_mem(&p->y);
+}
+
+void* cn_malloc(unsigned long size);
+
+int main(void) {
+  int x = 5;
+  int abs_x = abs_mem(&x);
+
+  struct tuple *t = cn_malloc(sizeof(struct tuple));
+  t->x = 4;
+  t->y = 3;
+  int abs_t = abs_y(t);
 }
